96e013785f579f67700d7ddec2bd807f0a9602c4
Hadrian Hughes <h.hughes@amaze.com>
2019-10-21 22:59:19 +0100

    Change angle of sled to match line

--- index.js
+++ index.js
@@ -2,30 +2,43 @@ const canvas = document.getElementById('root');
 const ctx = canvas.getContext('2d');
 
 const CANVAS_SIZE = 200;
+const POINT_X = CANVAS_SIZE + 200;
+const ACTIVE_INDEX = 1;
+const SPEED = 0.01;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-const ORIGIN = 0;
-const MAX_DISTANCE = 400;
-const SPEED = 5;
-
-const randomBetween = (lower, upper) => {
-  const rand = Math.floor(Math.random() * (upper - lower));
-  return rand + lower;
-};
-
-const generateCoord = () => randomBetween(ORIGIN + CANVAS_SIZE / 2, MAX_DISTANCE);
-const generatePoint = () => ({ x: generateCoord(), y: generateCoord() });
-
-
 let points = [];
+let percentCounter = 0.5;
+
+const random = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
+
+const generatePoint = (x, y) => ({
+  x: isNaN(x) ? POINT_X : x,
+  y: isNaN(y) ? random(CANVAS_SIZE) : y
+});
+
+function CubicN (pct, a, b, c, d) {
+  const t2 = pct * pct;
+  const t3 = t2 * pct;
+  return a + (-a * 3 + pct * (3 * a - a * pct)) * pct
+  + (3 * b + pct * (-6 * b + b * 3 * pct)) * pct
+  + (c * 3 - c * 3 * pct) * t2
+  + d * t3;
+}
+
+function getCubicBezierXYatPercent (start, end, cp1, cp2, percent) {
+  const x = CubicN(percent, start.x, cp1.x, cp2.x, end.x);
+  const y = CubicN(percent, start.y, cp1.y, cp2.y, end.y);
+  return { x, y };
+}
 
 function init () {
-  // Generate points
   points = [
-    { x: -(CANVAS_SIZE / 2), y: randomBetween(-(CANVAS_SIZE / 2), CANVAS_SIZE / 2) },
-    { x: CANVAS_SIZE / 2, y: randomBetween(ORIGIN, MAX_DISTANCE) },
+    generatePoint(-CANVAS_SIZE),
+    generatePoint(0),
+    generatePoint(CANVAS_SIZE),
     generatePoint()
   ];
 
@@ -40,30 +53,29 @@ function loop () {
 }
 
 function update () {
-  const p = points[0];
-  const pp = points[1];
+  percentCounter += SPEED;
+  if (percentCounter > 1) {
+    points = [ ...points.slice(1), generatePoint() ];
+    percentCounter = SPEED;
+  }
+
+  const p = points[ACTIVE_INDEX];
+  const pp = points[ACTIVE_INDEX + 1];
   const mid = { x: (p.x + pp.x) / 2, y: (p.y + pp.y) / 2 };
   const cp1 = { x: (mid.x + p.x) / 2, y: p.y };
   const cp2 = { x: (mid.x + pp.x) / 2, y: pp.y };
-  const sledOnLine = getCubicBezierXYatPercent(p, pp, cp1, cp2, 0.5);
+  const sledOnLine = getCubicBezierXYatPercent(p, pp, cp1, cp2, percentCounter);
+  const differenceY = (CANVAS_SIZE / 2) - sledOnLine.y;
+  const differenceX = (CANVAS_SIZE / 2) - sledOnLine.x;
 
-  points = points.map(point => ({ x: point.x - sledOnLine.x, y: point.y - sledOnLine.y }));
+  points = points.map(point => ({ x: point.x + differenceX, y: point.y + differenceY }));
 }
 
 function render () {
   canvas.width = canvas.width;
 
-  const xOffset = CANVAS_SIZE / 2;
-  const yOffset = CANVAS_SIZE / 2;
-
-  ctx.fillStyle = 'red';
-  ctx.beginPath();
-  ctx.arc(xOffset, yOffset, 5, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.closePath();
-  ctx.fillStyle = 'black';
-
-  ctx.moveTo(points[0].x + xOffset, points[0].y + yOffset);
+  // Render terrain
+  ctx.moveTo(points[0].x, points[0].y);
   for (let i = 0;i < points.length - 1;i += 1) {
     const p = points[i];
     const pp = points[i + 1];
@@ -71,32 +83,32 @@ function render () {
     const cp1 = { x: (mid.x + p.x) / 2, y: p.y };
     const cp2 = { x: (mid.x + pp.x) / 2, y: pp.y };
 
-    ctx.bezierCurveTo(
-      cp1.x + xOffset,
-      cp1.y + yOffset,
-      cp2.x + xOffset,
-      cp2.y + yOffset,
-      pp.x + xOffset,
-      pp.y + yOffset
-    );
+    ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, pp.x, pp.y);
+    ctx.stroke();
   }
-  ctx.stroke();
+
+  ctx.closePath();
+
+  // Render sled
+  const p = points[ACTIVE_INDEX];
+  const pp = points[ACTIVE_INDEX + 1];
+  const mid = { x: (p.x + pp.x) / 2, y: (p.y + pp.y) / 2 };
+  const cp1 = { x: (mid.x + p.x) / 2, y: p.y };
+  const cp2 = { x: (mid.x + pp.x) / 2, y: pp.y };
+
+  const p1 = getCubicBezierXYatPercent(p, pp, cp1, cp2, percentCounter);
+  const p2 = getCubicBezierXYatPercent(p, pp, cp1, cp2, percentCounter + SPEED);
+  const dy = p2.y - p1.y;
+  const dx = p2.x - p1.x;
+  const angle = Math.atan2(dy, dx);
+
+  ctx.translate(CANVAS_SIZE / 2, CANVAS_SIZE / 2);
+  ctx.rotate(angle);
+  ctx.translate(-CANVAS_SIZE / 2, -CANVAS_SIZE / 2);
+  ctx.beginPath();
+  ctx.fillStyle = 'red';
+  ctx.fillRect(CANVAS_SIZE / 2 - 5, CANVAS_SIZE / 2 - 10, 10, 10);
+  ctx.closePath();
 }
 
 init();
-
-function getCubicBezierXYatPercent (start, end, cp1, cp2, percent) {
-  const x = CubicN(percent, start.x, cp1.x, cp2.x, end.x);
-  const y = CubicN(percent, start.y, cp1.y, cp2.y, end.y);
-  return { x, y };
-}
-
-// cubic helper formula at percent distance
-function CubicN (pct, a, b, c, d) {
-  const t2 = pct * pct;
-  const t3 = t2 * pct;
-  return a + (-a * 3 + pct * (3 * a - a * pct)) * pct
-  + (3 * b + pct * (-6 * b + b * 3 * pct)) * pct
-  + (c * 3 - c * 3 * pct) * t2
-  + d * t3;
-}
